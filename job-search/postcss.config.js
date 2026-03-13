b958a083d0188fe0e2216d578d0635d03e20ab1f
ngohungphuc <ngohungphuc95@gmail.com>
2026-03-13 23:43:01 +0100

    update style

--- job-search/postcss.config.js
+++ job-search/postcss.config.js
@@ -9,6 +9,11 @@ export default {
       fontFamily: {
         sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'brand-gray-1': '#dadce0',
+        'brand-blue-1': '#1967d2',
+        'brand-green-1': '#137333',
+      },
     },
   },
   content: ['./src/**/*.{vue,js,ts}'],
