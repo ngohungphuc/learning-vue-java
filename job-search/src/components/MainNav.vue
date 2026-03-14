db274960d341722300aee0f5cf3cbe8d47f8c3bf
ngohungphuc <ngohungphuc95@gmail.com>
2026-03-14 16:45:15 +0100

    update nav

--- job-search/src/components/MainNav.vue
+++ job-search/src/components/MainNav.vue
@@ -2,10 +2,15 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
-        <h2 class="ml-8 flex h-full items-center">
-          Developed by {{ author.firstName }} {{ author.lastName }}
-        </h2>
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li v-for="menuItem in menuItems" :key="menuItem" class="ml-9 h-full first:ml-0">
+              <a href="" class="flex h-full items-center py-2.5">{{ menuItem }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -16,11 +21,9 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Company name',
-      author: {
-        firstName: 'Tony',
-        lastName: 'Ngo',
-      },
+      company: 'Test company',
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Locations', 'Life at Google Corp', 'How we hire', 'Students', 'Jobs'],
     }
   },
 }
