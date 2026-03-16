584fe9aa209ddf55969e09a7c506a8479bbc5076
ngohungphuc <ngohungphuc95@gmail.com>
2026-03-16 21:55:32 +0100

    Update

---
+++ job-search/src/components/TheHeadline.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <h1>{{ action }} for everyone</h1>
+    <h2>Find your next job at</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TheHeadLine',
+  data() {
+    return {
+      action: 'Build',
+      interval: null,
+    }
+  },
+  created() {
+    this.changedTitle()
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  },
+  methods: {
+    changedTitle() {
+      this.interval = setInterval(() => {
+        const actions = ['Build', 'Create', 'Design', 'Code']
+        const currentActionindex = actions.indexOf(this.action)
+        const nextActionIndex = (currentActionindex + 1) % 4
+        const nextAction = actions[nextActionIndex]
+        this.action = nextAction
+      }, 500)
+    },
+  },
+}
+</script>
