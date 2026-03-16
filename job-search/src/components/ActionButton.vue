081c82e62f8856e3fca9c13b84aad24db2e435ac
ngohungphuc <ngohungphuc95@gmail.com>
2026-03-16 20:57:54 +0100

    clean up

--- job-search/src/components/ActionButton.vue
+++ job-search/src/components/ActionButton.vue
@@ -7,10 +7,12 @@
 <script>
 export default {
   name: 'ActionButton',
-  props: ['text', 'isPrimary'],
+  props: ['text', 'type'],
   computed: {
     buttonClass() {
-      return { primary: this.isPrimary, secondary: !this.isPrimary }
+      return {
+        [this.type]: true,
+      }
     },
   },
 }
