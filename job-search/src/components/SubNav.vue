3ffeaaeb9e94801aa3c8b8631223df553b74102a
ngohungphuc <ngohungphuc95@gmail.com>
2026-03-16 22:08:29 +0100

    cleanup

--- job-search/src/components/SubNav.vue
+++ job-search/src/components/SubNav.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
     <div class="flex h-full items-center px-8">
-      <div><font-awesome-icon :icon="['fas', 'search']" class="mr-3"></font-awesome-icon></div>
-      <span><span class="text-brand-green-1"> x</span> jobs matched</span>
+      <div v-if="onJobResultsPage">
+        <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+        <span><span class="text-brand-green-1">1653</span> jobs matched</span>
+      </div>
     </div>
   </div>
 </template>
@@ -10,5 +12,10 @@
 <script>
 export default {
   name: 'Subnav',
+  data() {
+    return {
+      onJobResultsPage: true,
+    }
+  },
 }
 </script>
--- job-search/src/components/TheHeadline.vue
+++ job-search/src/components/TheHeadline.vue
@@ -1,35 +1,63 @@
 <template>
   <section>
-    <h1>{{ action }} for everyone</h1>
-    <h2>Find your next job at</h2>
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="actionClasses">{{ action }}</span>
+      <br />
+      for everyone
+    </h1>
+    <h2 class="text-3xl font-light">Find your next job at Bobo Corp.</h2>
   </section>
 </template>
 
 <script>
+import nextElementInList from '@/components/utils/nextElementInList'
+
 export default {
-  name: 'TheHeadLine',
+  name: 'TheHeadline',
   data() {
     return {
       action: 'Build',
       interval: null,
     }
   },
+  computed: {
+    actionClasses() {
+      return {
+        [this.action.toLowerCase()]: true,
+      }
+    },
+  },
   created() {
-    this.changedTitle()
+    this.changeTitle()
   },
   beforeUnmount() {
     clearInterval(this.interval)
   },
   methods: {
-    changedTitle() {
+    changeTitle() {
       this.interval = setInterval(() => {
         const actions = ['Build', 'Create', 'Design', 'Code']
-        const currentActionindex = actions.indexOf(this.action)
-        const nextActionIndex = (currentActionindex + 1) % 4
-        const nextAction = actions[nextActionIndex]
-        this.action = nextAction
-      }, 500)
+        this.action = nextElementInList(actions, this.action)
+      }, 3000)
     },
   },
 }
 </script>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+
+.create {
+  color: #34a853;
+}
+
+.design {
+  color: #f9ab00;
+}
+
+.code {
+  color: #d93025;
+}
+</style>
