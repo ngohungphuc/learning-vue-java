89858fdb7c9f9584116b0a650e5a3f31a8e16a46
ngohungphuc <ngohungphuc95@gmail.com>
2026-03-18 22:42:30 +0100

    update demo

--- java/src/DemoJDBC.java
+++ java/src/DemoJDBC.java
@@ -5,10 +5,17 @@ public class DemoJDBC { // No 'throw' here
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "x";
-
+        String sql = "select sname from student where sid = 1";
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass); // Added .getConnection
 
         System.out.println("Connection established");
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        rs.next();
+        String name = rs.getString("sname");
+        System.out.println("Student name is" + name);
+        con.close();
+        System.out.println("Connection closed");
     }
 }
