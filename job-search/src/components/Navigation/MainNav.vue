a82cb6e84287ebdbf6731c9381c569c1f36597ed
ngohungphuc <ngohungphuc95@gmail.com>
2026-03-18 09:22:26 +0100

    fix router link

--- job-search/src/components/Navigation/MainNav.vue
+++ job-search/src/components/Navigation/MainNav.vue
@@ -2,12 +2,16 @@
   <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <router-link to="{name: 'Home'}" class="flex h-full items-center text-xl">{{
+          company
+        }}</router-link>
 
         <nav class="ml-12 h-full">
           <ul class="flex h-full list-none">
             <li v-for="menuItem in menuItems" :key="menuItem" class="ml-9 h-full first:ml-0">
-              <a href="" class="flex h-full items-center py-2.5">{{ menuItem }}</a>
+              <router-link href="" class="flex h-full items-center py-2.5">{{
+                menuItem
+              }}</router-link>
             </li>
           </ul>
         </nav>
@@ -37,7 +41,6 @@ export default {
   data() {
     return {
       company: 'Company name',
-      url: 'https://careers.google.com',
       menuItems: ['Teams', 'Locations', 'Life at Google Corp', 'How we hire', 'Students', 'Jobs'],
       isLoggedIn: false,
     }
