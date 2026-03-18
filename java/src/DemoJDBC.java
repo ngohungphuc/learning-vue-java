f588f2bcfe195388a05cd11cb07a6a8b8dd1d691
ngohungphuc <ngohungphuc95@gmail.com>
2026-03-18 22:27:10 +0100

    update jdbc

---
+++ java/src/DemoJDBC.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+public class DemoJDBC throw Exception {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql:demo";
+        String uname = "postgres";
+        String pass = "0000";
+
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager(uri: "", user: "", password:"")
+    }
+}
+
