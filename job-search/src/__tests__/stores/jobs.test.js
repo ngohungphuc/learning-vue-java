9af59fc64e642ae2b2b07f7d5188aa5be15e402e
ngohungphuc <ngohungphuc95@gmail.com>
2026-03-22 22:14:15 +0100

    update

--- job-search/src/__tests__/stores/jobs.test.js
+++ job-search/src/__tests__/stores/jobs.test.js
@@ -1,32 +1,92 @@
-import { createPinia, setActivePinia } from "pinia";
-import axios from "axios";
+import { createPinia, setActivePinia } from 'pinia'
+import axios from 'axios'
 
-import { useJobsStore } from "@/stores/jobs";
+import { useJobsStore } from '@/stores/jobs'
+import { useUserStore } from '@/stores/user'
 
-vi.mock("axios");
+vi.mock('axios')
 
-describe("state", () => {
+describe('state', () => {
   beforeEach(() => {
-    setActivePinia(createPinia());
-  });
+    setActivePinia(createPinia())
+  })
 
-  it("stores job listings", () => {
-    const store = useJobsStore();
-    expect(store.jobs).toEqual([]);
-  });
-});
+  it('stores job listings', () => {
+    const store = useJobsStore()
+    expect(store.jobs).toEqual([])
+  })
+})
 
-describe("actions", () => {
+describe('actions', () => {
   beforeEach(() => {
-    setActivePinia(createPinia());
-  });
+    setActivePinia(createPinia())
+  })
 
-  describe("FETCH_JOBS", () => {
-    it("makes API request and stores received jobs", async () => {
-      axios.get.mockResolvedValue({ data: ["Job 1", "Job 2"] });
-      const store = useJobsStore();
-      await store.FETCH_JOBS();
-      expect(store.jobs).toEqual(["Job 1", "Job 2"]);
-    });
-  });
-});
+  describe('FETCH_JOBS', () => {
+    it('makes API request and stores received jobs', async () => {
+      axios.get.mockResolvedValue({ data: ['Job 1', 'Job 2'] })
+      const store = useJobsStore()
+      await store.FETCH_JOBS()
+      expect(store.jobs).toEqual(['Job 1', 'Job 2'])
+    })
+  })
+})
+
+describe('getters', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  describe('UNIQUE_ORGANIZATIONS', () => {
+    it('finds unique organizations from list of jobs', () => {
+      const store = useJobsStore()
+      store.jobs = [
+        { organization: 'Google' },
+        { organization: 'Amazon' },
+        { organization: 'Google' },
+      ]
+
+      const result = store.UNIQUE_ORGANIZATIONS
+
+      expect(result).toEqual(new Set(['Google', 'Amazon']))
+    })
+  })
+
+  describe('FILTERED_JOBS_BY_ORGANIZATIONS', () => {
+    it('identifies jobs that are associated with the given organizations', () => {
+      const jobsStore = useJobsStore()
+      jobsStore.jobs = [
+        { organization: 'Google' },
+        { organization: 'Amazon' },
+        { organization: 'Microsoft' },
+      ]
+      const userStore = useUserStore()
+      userStore.selectedOrganizations = ['Google', 'Microsoft']
+
+      const result = jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS
+
+      expect(result).toEqual([{ organization: 'Google' }, { organization: 'Microsoft' }])
+    })
+
+    describe('when the user has not selected any organizations', () => {
+      it('returns all jobs', () => {
+        const jobsStore = useJobsStore()
+        jobsStore.jobs = [
+          { organization: 'Google' },
+          { organization: 'Amazon' },
+          { organization: 'Microsoft' },
+        ]
+        const userStore = useUserStore()
+        userStore.selectedOrganizations = []
+
+        const result = jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS
+
+        expect(result).toEqual([
+          { organization: 'Google' },
+          { organization: 'Amazon' },
+          { organization: 'Microsoft' },
+        ])
+      })
+    })
+  })
+})
--- job-search/src/stores/jobs.js
+++ job-search/src/stores/jobs.js
@@ -1,17 +1,24 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia'
 
-import getJobs from "@/api/getJobs";
+import getJobs from '@/api/getJobs'
 
-export const FETCH_JOBS = "FETCH_JOBS";
-
-export const useJobsStore = defineStore("jobs", {
+export const FETCH_JOBS = 'FETCH_JOBS'
+export const UNIQUE_ORGANIZATIONS = 'UNIQUE_ORGANIZATIONS'
+export const useJobsStore = defineStore('jobs', {
   state: () => ({
     jobs: [],
   }),
   actions: {
     async [FETCH_JOBS]() {
-      const jobs = await getJobs();
-      this.jobs = jobs;
+      const jobs = await getJobs()
+      this.jobs = jobs
     },
   },
-});
+  getters: {
+    [UNIQUE_ORGANIZATIONS](state) {
+      const uniqueOrganizations = new Set()
+      state.jobs.forEach((job) => uniqueOrganizations.add(job.organization))
+      return uniqueOrganizations
+    },
+  },
+})
