9114e8af47c0fb47b8723ecf1b7fd0b6cfa89224
ngohungphuc <ngohungphuc95@gmail.com>
2026-03-31 21:03:04 +0200

    update route

--- job-search/src/__tests__/components/JobSearchForm.test.js
+++ job-search/src/__tests__/components/JobSearchForm.test.js
@@ -1,44 +1,44 @@
-import { render, screen } from "@testing-library/vue";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from '@testing-library/vue'
+import userEvent from '@testing-library/user-event'
 
-import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
+import { useRouter } from 'vue-router'
+vi.mock('vue-router')
 
-describe("JobSearchForm", () => {
-  describe("when user submits form", () => {
+import JobSearchForm from '@/components/JobSearch/JobSearchForm.vue'
+
+describe('JobSearchForm', () => {
+  describe('when user submits form', () => {
     it("directs user to job results page with user's search parameters", async () => {
-      const push = vi.fn();
-      const $router = { push };
+      const push = vi.fn()
+      useRouter.mockReturnValue({ push })
 
       render(JobSearchForm, {
         global: {
-          mocks: {
-            $router,
-          },
           stubs: {
             FontAwesomeIcon: true,
           },
         },
-      });
+      })
 
-      const roleInput = screen.getByRole("textbox", {
+      const roleInput = screen.getByRole('textbox', {
         name: /role/i,
-      });
-      await userEvent.type(roleInput, "Vue Developer");
+      })
+      await userEvent.type(roleInput, 'Vue Developer')
 
-      const locationInput = screen.getByRole("textbox", {
+      const locationInput = screen.getByRole('textbox', {
         name: /where?/i,
-      });
-      await userEvent.type(locationInput, "Dallas");
+      })
+      await userEvent.type(locationInput, 'Dallas')
 
-      const submitButton = screen.getByRole("button", {
+      const submitButton = screen.getByRole('button', {
         name: /search/i,
-      });
-      await userEvent.click(submitButton);
+      })
+      await userEvent.click(submitButton)
 
       expect(push).toHaveBeenCalledWith({
-        name: "JobResults",
-        query: { role: "Vue Developer", location: "Dallas" },
-      });
-    });
-  });
-});
+        name: 'JobResults',
+        query: { role: 'Vue Developer', location: 'Dallas' },
+      })
+    })
+  })
+})
