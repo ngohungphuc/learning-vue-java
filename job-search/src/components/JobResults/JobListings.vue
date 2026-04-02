5fd8a99e7a96b18b4d42b7579ff7a3535674bb75
ngohungphuc <ngohungphuc95@gmail.com>
2026-04-02 22:44:41 +0200

    cleanup

--- job-search/src/components/JobResults/JobListings.vue
+++ job-search/src/components/JobResults/JobListings.vue
@@ -35,34 +35,27 @@
 <script setup>
 import { computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+
 import JobListing from '@/components/JobResults/JobListing.vue'
 import { useJobsStore } from '@/stores/jobs'
 
-const route = useRoute()
+import usePreviousAndNextPages from '@/composables/usePreviousAndNextPages'
+
 const jobsStore = useJobsStore()
+onMounted(jobsStore.FETCH_JOBS)
 
+const FILTERED_JOBS = computed(() => jobsStore.FILTERED_JOBS)
+
+const route = useRoute()
 const currentPage = computed(() => Number.parseInt(route.query.page || '1'))
+const maxPage = computed(() => Math.ceil(FILTERED_JOBS.value.length / 10))
 
-const previousPage = computed(() => {
-  const prevPage = currentPage.value - 1
-  const firstPage = 1
-  return prevPage >= firstPage ? prevPage : undefined
-})
-
-const nextPage = computed(() => {
-  const nextPg = currentPage.value + 1
-  const maxPage = Math.ceil(jobsStore.FILTERED_JOBS.length / 10)
-  return nextPg <= maxPage ? nextPg : undefined
-})
+const { previousPage, nextPage } = usePreviousAndNextPages(currentPage, maxPage)
 
 const displayedJobs = computed(() => {
   const pageNumber = currentPage.value
   const firstJobIndex = (pageNumber - 1) * 10
   const lastJobIndex = pageNumber * 10
-  return jobsStore.FILTERED_JOBS.slice(firstJobIndex, lastJobIndex)
-})
-
-onMounted(() => {
-  jobsStore.FETCH_JOBS()
+  return FILTERED_JOBS.value.slice(firstJobIndex, lastJobIndex)
 })
 </script>
