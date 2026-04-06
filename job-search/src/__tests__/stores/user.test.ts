6ee69f7d6bec7592f635761ced26a988557aa72a
ngohungphuc <ngohungphuc95@gmail.com>
2026-04-06 10:27:41 +0200

    enhacement

--- job-search/src/__tests__/stores/user.test.ts
+++ job-search/src/__tests__/stores/user.test.ts
@@ -26,7 +26,7 @@ describe('actions', () => {
   describe('loginUser', () => {
     it('logs the user in', () => {
       const store = useUserStore()
-      store.loginUser()
+      store.LOGIN_USER()
       expect(store.isLoggedIn).toBe(true)
     })
   })
@@ -52,6 +52,21 @@ describe('actions', () => {
       const store = useUserStore()
       store.ADD_SELECTED_DEGREES(["Bachelor's", "Master's"])
       expect(store.selectedDegrees).toEqual(["Bachelor's", "Master's"])
+    })
+  })
+
+  describe('CLEAR_USER_JOB_FILTER_SELECTIONS', () => {
+    it('removes all job filters that user has chosen', () => {
+      const store = useUserStore()
+      store.selectedDegrees = ['Random degree']
+      store.selectedJobTypes = ['Random job type']
+      store.selectedOrganizations = ['Random organization']
+
+      store.CLEAR_USER_JOB_FILTER_SELECTIONS()
+
+      expect(store.selectedDegrees).toEqual([])
+      expect(store.selectedJobTypes).toEqual([])
+      expect(store.selectedOrganizations).toEqual([])
     })
   })
 })
